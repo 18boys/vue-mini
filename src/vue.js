b35d9112febdb1efdb2686ccd350c07ff455d557
lishuai41 <lishuai41@meituan.com>
2023-02-07 21:38:23 +0800

    fix: 解决副作用收集的时候又访问同一属性导致死循环的问题

--- src/vue.js
+++ src/vue.js
@@ -3,6 +3,7 @@ const bucket = new Map();
 
 const effectStack = [];
 let isCollecting = true;
+let activeEffect = null;
 /**
  * 将对象转化为响应式对象
  * @param data
@@ -14,7 +15,9 @@ function ref(data) {
     get(target, key) {
       // 依赖收集, 将副作用收集到副作用bucket中
       if (!effectStack.length) return target[key];
-      const activeEffect = effectStack[effectStack.length - 1];
+      const nextEffect = effectStack[effectStack.length - 1];
+      if(nextEffect === activeEffect) return target[key]; // 新触发的和上一次的副作用函数一样,说明是在当前收集副作用的时候又触发了副作用收集
+      activeEffect = nextEffect;
       if (!bucket.get(target)) { // 注意这里需要用get而不是能直接设置属性,否则key不为对象
         bucket.set(target, new Map());
       }
@@ -26,7 +29,7 @@ function ref(data) {
         depsMap.get(key).deps.push(activeEffect);
         p.__raw__ = target;
         if (typeof target[key] === 'object') {
-          p[key] = ref(target[key]); // 直接在get中设置会导致死循环,因此收集过程不触发副作用
+          p[key] = ref(target[key]); // 直接在get中设置会导致死循环,因此收集过程不触发副作用,设置isCollecting
         }
       }
       return target[key];
@@ -49,6 +52,7 @@ function effect(fn) {
   fn(); // 执行收集动作
   isCollecting = false;
   effectStack.pop();
+  activeEffect = null;
 }
 
 function computed(getter) {
